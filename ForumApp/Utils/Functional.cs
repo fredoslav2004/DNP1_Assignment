f72abbc9891c6974b962780c6ce74df81e5ad311
Eduard Fekete <edko.fek@gmail.com>
2025-11-12 20:22:55 +0100

    [Add] AsyncResource Monad

---
+++ ForumApp/Utils/Functional.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Utils;
+
+public static class Functional
+{
+    public class AsyncResource<T>
+    {
+        public T? Resource { get; set; }
+        public bool IsLoading { get; set; } = false;
+        public Exception? Error { get; set; }
+
+        public void SetValue(T resource)
+        {
+            Resource = resource;
+            IsLoading = false;
+            Error = null;
+        }
+
+        public void SetError(Exception ex)
+        {
+            Resource = default;
+            IsLoading = false;
+            Error = ex;
+        }
+
+        public void SetInvalid(string message)
+        {
+            Resource = default;
+            IsLoading = false;
+            Error = new Exception(message);
+        }
+
+        public static implicit operator T(AsyncResource<T> asyncResource)
+        {
+            return asyncResource.Resource!;
+        }
+
+        public bool HasValue()
+        {
+            return Resource != null && Error == null && !IsLoading;
+        }
+    }
+}
